9c7217c7f4d327c4dc79f310bcc18ac52b67268b
flyingpimonster <flyingpimonster@flyingpimonster.net>
2018-01-23 22:10:29 -0600

    🐛Don't crash, just log an error here

--- js/bundle.js
+++ js/bundle.js
@@ -4,6 +4,9 @@
 // THERE IS A REASON THIS PROJECT USES WEBPACK. IN NON-DEV BUILDS, BUNDLE.JS IS
 // THE OUTPUT OF WEBPACK, NOT THIS FILE
 
-if(process.env.NODE_ENV !== "development") throw "Development code used in production environment!"
+if(process.env.NODE_ENV !== "development") {
+    // throw "Development code used in production environment!"
+    console.log("WARNING! Development code used in production environment!")
+}
 
 require("../src/environment/desktop.js")
